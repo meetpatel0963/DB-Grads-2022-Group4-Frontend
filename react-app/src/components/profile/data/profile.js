c7be95d79b6733c6899b14338babcd7b981f5067
anu <emily.eldose@gmail.com>
2022-08-09 19:23:20 +0530

    added profile component

---
+++ react-app/src/components/profile/data/profile.js
@@ -0,0 +1,14 @@
+
+export default 
+  { 
+    name: "Sophie B.",
+    email: "sophie.b@db.com",
+    dept: "Operations Team",
+    location: "DB Pune",
+    // action: {
+    //   type: "internal",
+    //   route: "/pages/profile/profile-overview",
+    //   color: "info", 
+    // },
+
+  };
