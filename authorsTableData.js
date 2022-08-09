8cf15aed3ff950f88e635a6718ec83723ea630cc
Raquel23 <51993235+chitralekha2312@users.noreply.github.com>
2022-08-09 13:43:59 +0530

    Update authorsTableData.js

--- authorsTableData.js
+++ authorsTableData.js
@@ -22,9 +22,9 @@ import MDAvatar from "components/MDAvatar";
 import MDBadge from "components/MDBadge";
 
 // Images
-import team2 from "assets/images/team-2.jpg";
-import team3 from "assets/images/team-3.jpg";
-import team4 from "assets/images/team-4.jpg";
+// import team2 from "assets/images/team-2.jpg";
+// import team3 from "assets/images/team-3.jpg";
+// import team4 from "assets/images/team-4.jpg";
 
 export default function data() {
   const Author = ({ image, name, email }) => (
